f7091eb1aed9b44ca28d2a366fc37ca2af75e0f9
Connor Wenck <none@example.com>
2021-02-15 13:51:02 -0500

    Cleaned up now unused code

--- gif-converter.py
+++ gif-converter.py
@@ -3,10 +3,7 @@
 from PIL import Image
 import csv
 import sys
-# import math
 
-# COLOR_STEP = 64
-# COLOR_DELIMITER = ','
 PIXEL_DELIMITER = ','
 
 
@@ -33,62 +30,6 @@ def extract_frames(gif):
         frames.append(frame)
 
     return frames
-
-
-# def approximate_value(val, step):
-#     mod = val % step
-#     if mod == 0:
-#         return val
-
-#     delta_up = step - mod
-#     delta_down = mod
-
-#     if delta_up < delta_down:
-#         return val + delta_up
-#     else:
-#         return val - delta_down
-
-
-# def round_value(val, step):
-#     mod = val % step
-#     if mod == 0:
-#         return (val, val)
-
-#     delta_up = step - mod
-#     delta_down = mod
-#     return (val + delta_up, val - delta_down)
-
-
-# def dist(a, b):
-#     (x1, y1, z1) = a
-#     (x2, y2, z2) = b
-
-#     x_diff_squared = (x1 - x2) ** 2
-#     y_diff_squared = (y1 - y2) ** 2
-#     z_diff_squared = (z1 - z2) ** 2
-
-#     return math.sqrt(x_diff_squared + y_diff_squared + z_diff_squared)
-
-# def adjacent_colors(color):
-#     (r, g, b) = color
-
-#     result = []
-#     for adj_r in round_value(r, COLOR_STEP):
-#         for adj_b in round_value(g, COLOR_STEP):
-#             for adj_g in round_value(b, COLOR_STEP):
-#                 result.append(adj_r, adj_b, adj_g)
-
-#     return result
-
-
-# def round_color(color, color_step):
-#     (r, g, b) = color
-#     return (approximate_value(r, color_step), approximate_value(g, color_step), approximate_value(b, color_step))
-
-
-# def color_to_str(color):
-#     (r, g, b) = color
-#     return str(r) + COLOR_DELIMITER + str(g) + COLOR_DELIMITER + str(b)
 
 
 def to_csv(frames, name):
